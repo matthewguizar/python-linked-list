2c69bceecbc68a557192bccc4acc2878f77af1ab
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-07 15:25:31 -0800

    working append method for linked-list ex

--- test.py
+++ test.py
@@ -16,3 +16,21 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(1)
+
+my_linked_list.append(2)
+
+my_linked_list.print_list()
