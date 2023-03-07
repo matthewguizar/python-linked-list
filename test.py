3e248ee257a7634f893fec73a8f050036e71a37e
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-07 15:49:36 -0800

    pop

--- test.py
+++ test.py
@@ -28,9 +28,31 @@ class LinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+
+        temp = self.head
+        pre = self.head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
 
 my_linked_list = LinkedList(1)
 
 my_linked_list.append(2)
 
 my_linked_list.print_list()
+
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
