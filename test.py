4044f690f19d3240bc270f6624c08e1180955b72
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-08 17:48:40 -0800

    added reverse

--- test.py
+++ test.py
@@ -111,14 +111,26 @@ class LinkedList:
         self.length -= 1
         return temp
 
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
 
-my_linked_list = LinkedList(11)
 
+my_linked_list = LinkedList(1)
+
+my_linked_list.append(2)
 my_linked_list.append(3)
-my_linked_list.append(23)
-my_linked_list.append(7)
+my_linked_list.append(4)
 
-print(my_linked_list.remove(2), '\n')
+my_linked_list.reverse()
 
 
 my_linked_list.print_list()
